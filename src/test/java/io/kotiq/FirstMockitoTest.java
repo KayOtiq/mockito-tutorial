7ce84c740b3ceb61406b57352694980e32048d5c
Lynda Montanez <lyndamontanez@gmail.com>
2019-11-12 07:37:23 -0800

    initial commit for learning how to use mockito

---
+++ src/test/java/io/kotiq/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+package io.kotiq;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstMockitoTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
